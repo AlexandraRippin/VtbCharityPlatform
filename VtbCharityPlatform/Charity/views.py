e932caf6186b62402dcada47d840fe9f16b16463
SerialN <>
2025-11-14 21:10:01 +0300

    Первая страница уху. Пока это затычка чисто для проверки

--- VtbCharityPlatform/Charity/views.py
+++ VtbCharityPlatform/Charity/views.py
@@ -1,3 +1,14 @@
+from django.db.models.aggregates import Sum
 from django.shortcuts import render
 
+from .models import Project, Donation
+
+
 # Create your views here.
+def index(request):
+    total_amount = Donation.objects.aggregate(Sum('amount'))['amount__sum'] or 0
+    projects = Project.objects.all()
+    return render(request, 'index.html', context={
+        'total_amount': total_amount,
+        'projects': projects,
+    })
