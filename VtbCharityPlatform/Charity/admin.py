e74be65fd7690632c6bbf81c46e385fe4e98585e
SerialN <>
2025-11-14 21:19:37 +0300

    Плюс админка для проверки

--- VtbCharityPlatform/Charity/admin.py
+++ VtbCharityPlatform/Charity/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from Charity.models import Project, Donation
+
+
 # Register your models here.
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', "target_amount", 'current_amount')
+
+
+@admin.register(Donation)
+class DonationAdmin(admin.ModelAdmin):
+    list_display = ('project', 'amount', 'donor_name')
--- VtbCharityPlatform/Charity/models.py
+++ VtbCharityPlatform/Charity/models.py
@@ -12,5 +12,5 @@ class Project(models.Model):
 class Donation(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     amount = models.DecimalField(decimal_places=2, max_digits=10)
-    donorName = models.CharField(max_length=100)
+    donor_name = models.CharField(max_length=100)
     date = models.DateField(auto_now_add=True)
